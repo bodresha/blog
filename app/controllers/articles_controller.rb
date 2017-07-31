aae0e3377643b2d6cedcaa82e2b56f7e33942b55
Meshal A.  Alenezi <mynamemishal@gmail.com>
2017-07-31 04:21:33 +0000

    add connection between user and articles also added debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,8 +26,9 @@ class ArticlesController < ApplicationController
   end
   
   def create 
-    
+    debugger 
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Your article has been created"
       redirect_to article_path(@article)
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730041100) do
+ActiveRecord::Schema.define(version: 20170731035130) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "decsription"
     t.datetime "creat_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
