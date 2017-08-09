f176899097917de6a6954a4f07d3170618cf7903
Meshal A.  Alenezi <mynamemishal@gmail.com>
2017-08-09 07:36:10 +0000

    add pagination to views, fixing the profile showen the oldest articles first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.all
+    @users = User.order("username").paginate(page: params[:page], per_page: 5)
+    
+    
   end
   
   def new
@@ -34,6 +36,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.order("created_at DESC").paginate(page: params[:page], per_page: 4)
+
   end
   
   private
